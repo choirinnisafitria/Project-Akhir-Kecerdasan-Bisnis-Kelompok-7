2e835e6394fcc55f04c1fc6c0eb937922433967c
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-06-06 18:50:40 +0700

    Update bisnis.py

--- bisnis.py
+++ bisnis.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
@@ -39,8 +39,28 @@ with st.container():
         y = df['Status'].values
 
         scaler = MinMaxScaler()
-        scaled_X = scaler.fit_transform(X.values)  # Convert DataFrame to NumPy array
+        scaled_X = scaler.fit_transform(X.to_numpy())  # Convert DataFrame to NumPy array
         scaled_df = pd.DataFrame(scaled_X, columns=X.columns)
 
         st.subheader('Data Setelah Normalisasi')
         st.dataframe(scaled_df, width=600)
+
+    elif selected == 'Modeling':
+        df = pd.read_csv('https://raw.githubusercontent.com/BojayJaya/Project-Akhir-Kecerdasan-Bisnis-Kelompok-7/main/dataset.csv')
+
+        X = df.drop(columns=['Status'])
+        y = df['Status'].values
+
+        scaler = MinMaxScaler()
+        scaled_X = scaler.fit_transform(X.to_numpy())  # Convert DataFrame to NumPy array
+        scaled_df = pd.DataFrame(scaled_X, columns=X.columns)
+
+        X_train, X_test, y_train, y_test = train_test_split(scaled_df, y, test_size=0.2, random_state=1)
+
+        gaussian = GaussianNB()
+        gaussian.fit(X_train, y_train)
+        y_pred = gaussian.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+
+        st.subheader('Hasil Modeling dengan Naive Bayes')
+        st.write('Akurasi: {:.2f}%'.format(accuracy * 100))
