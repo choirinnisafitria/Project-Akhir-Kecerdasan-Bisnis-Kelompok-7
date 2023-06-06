66b0f403bd6bea704e1caec8b98e1198570fe3fb
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-06-06 11:09:30 +0700

    Update bisnis.py

--- bisnis.py
+++ bisnis.py
@@ -30,13 +30,13 @@ with st.container():
 
     if selected == 'Preprocessing':
         st.subheader('Normalisasi Data')
-        df = pd.read_csv('data.csv')  # Ganti dengan nama file dataset Anda
+        df = pd.read_csv('data.csv')
         
         st.subheader('Data Asli')
         st.dataframe(df, width=600)
 
-        X = df.drop(columns=['Status'])
-        y = df['Status'].values
+        X = df.drop(columns=['status'])
+        y = df['status'].values
 
         scaler = MinMaxScaler()
         scaled_X = scaler.fit_transform(X)
@@ -46,10 +46,10 @@ with st.container():
         st.dataframe(scaled_df, width=600)
 
     elif selected == 'Modeling':
-        df = pd.read_csv('data.csv')  # Ganti dengan nama file dataset Anda
+        df = pd.read_csv('data.csv')
 
-        X = df.drop(columns=['Status'])
-        y = df['Status'].values
+        X = df.drop(columns=['status'])
+        y = df['status'].values
 
         scaler = MinMaxScaler()
         scaled_X = scaler.fit_transform(X)
@@ -69,8 +69,8 @@ with st.container():
         st.subheader('Implementasi Prediksi Penyakit Diabetes')
         df = pd.read_csv('data.csv')  # Ganti dengan nama file dataset Anda
 
-        X = df.drop(columns=['Status'])
-        y = df['Status'].values
+        X = df.drop(columns=['status'])
+        y = df['status'].values
 
         scaler = MinMaxScaler()
         scaled_X = scaler.fit_transform(X)
